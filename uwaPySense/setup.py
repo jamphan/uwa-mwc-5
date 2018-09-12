06b1b6f3bcb02c961e762c61a55ecf6c4a8f68b9
Jamie K. Phan <jamie.kt.phan@gmail.com>
2018-09-12 15:42:17 +0800

    Add setup script

---
+++ uwaPySense/setup.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='uwaPySense',
+    packages=[],
+    version='0.0.1',
+    description='UWA py sensor',
+    author='Jamie Phan',
+    license='MIT',
+    author_email='jamie.kt.phan2gmail.com',
+    url='https://github.com/kjph/uwa-mwc-5.git',
+    keywords=['uwa'],
+)
