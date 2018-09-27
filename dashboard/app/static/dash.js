ae72d84595cd677c54ec4e7a1205bbc230d4e963
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-09-27 21:51:09 +0800

    add status icons

--- dashboard/app/static/dash.js
+++ dashboard/app/static/dash.js
@@ -41,12 +41,14 @@ function processData(){
 
         const percentFilled = (depth-lastValue)/(depth-thresh)*100;
         var barColor = 'bg-info';
+        var status = '<td><img class="bin-alert" width="18px" src = "/static/images/check.png"></td';;
         if(percentFilled > 60)
             barColor = 'bg-warning';
-        if(percentFilled > 85)
+        if(percentFilled > 85){
             barColor = 'bg-danger';
-        table.insertAdjacentHTML('beforeend', '<tr><td><b>'+num+'</b></td><td><div class="progress"><div class="progress-bar-striped progress-bar ' + barColor +'" role="progressbar" style="width: '+ percentFilled + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div></td><td>'+percentFilled+'%</td></tr>');
-        
+             status = '<td><img class="bin-alert" width="20px" src = "/static/images/exclamation-mark.png"></td';
+        }
+        table.insertAdjacentHTML('beforeend', '<tr><td><b>'+num+'</b></td><td><div class="progress"><div class="progress-bar-striped progress-bar ' + barColor +'" role="progressbar" style="width: '+ percentFilled + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div></td><td>'+percentFilled+'%</td>'+status+'</tr>');
         // add bin marker to google map
         var marker = new google.maps.Marker({
             position: {lat: bin["lat"],lng: bin["long"]},
