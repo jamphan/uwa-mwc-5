34e93cad615d70de605a7a8571458941a1e1e87f
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-09-28 16:26:25 +0800

    add data logs tab

--- dashboard/app/routes.py
+++ dashboard/app/routes.py
@@ -2,7 +2,7 @@ from app import server
 from flask import render_template
 from app import flaskApp
 from app.database.jsonDb import jsonDb
-
+import numpy
 
 @flaskApp.route('/')
 @flaskApp.route('/index')
@@ -16,11 +16,33 @@ def index():
 
     return render_template('home.html', data=data, bin_ids = bin_ids)
 
+@flaskApp.route('/datalog')
+def datalog():
+    db = jsonDb('app/data.json')
+
+    bin_ids = db.get_all_bins()
+    sensor_ids = db.get_all_sensors()
+
+    sensorData = db.data["sensors"];
+    binData = db.data["bins"];
+
+    database = db.data["data"]
+
+    return render_template('datalog.html',  bin_data=binData, bin_ids=bin_ids, 
+                                            sensor_ids = sensor_ids, 
+                                            sensor_data = sensorData, data = database)
+
 @flaskApp.route('/sensors')
 def sensors():
     db = jsonDb('app/data.json')
-    bin_ids = db.get_all_bins()
-    print(bin_ids)
-    data = db.data
 
-    return render_template('sensors.html', data=data, bin_ids = bin_ids)
+    bin_ids = db.get_all_bins()
+    sensor_ids = db.get_all_sensors()
+
+    sensorData = db.data["sensors"];
+    binData = db.data["bins"];
+
+    database = db.data["data"]
+    return render_template('sensors.html',  bin_data=binData, bin_ids=bin_ids, 
+                                            sensor_ids = sensor_ids, 
+                                            sensor_data = sensorData, data = database)
--- dashboard/app/static/sensors.js
+++ dashboard/app/static/sensors.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
 
-    $('#myTable').DataTable();
+    var table = $('#myTable').DataTable();
+    
+    num_bins = bin_ids.length;
+    for(var i = 0; i < num_bins; i++){
+        num_records = data[bin_ids[i]]["timestamps"].length;
+        for(var j = 0; j < num_records; j++){
+            var binID = bin_ids[i];
+            var bin = data[binID];
+            var time = bin["timestamps"][j];
+            var value = bin["values"][j];
+            var RSSI = bin["RSSI_values"][j];
+            var sensorID = bin_data[binID]["sensorid"];
+            var long = bin_data[binID]["lat"];
+            var lat = bin_data[binID]["long"];
 
+            console.log(sensor_data[sensorID]["type"]);
+            var sensorType = sensor_data[sensorID]["type"];
+            table.row.add(
+                [time, binID, sensorID, value, lat, long, RSSI, sensorType]
+            ).draw();
+            console.log(data[bin_ids[i]]["timestamps"][j])
+        }
+    }
 });
