ee06ec523bae8c6cf5ab40790b8feb04efe8c3fa
Jacquie Soon <jacqueline.gakmay.soon@gmail.com>
2018-09-29 17:07:15 +0800

    update bin map marker icons

--- dashboard/app/static/js/dash.js
+++ dashboard/app/static/js/dash.js
@@ -22,7 +22,8 @@ function processData(){
 
         var percentFilled = (depth-lastValue)/(depth-thresh)*100;
         var barColor = 'bg-info';
-        var status = '<td><img class="bin-alert" width="18px" src = "/static/images/check.png"></td';;
+        var status = '<td><img class="bin-alert" width="18px" src = "/static/images/check.png"></td';
+        var icon = 'static/images/trash-b.png';
         if(percentFilled < 0 || percentFilled > 100){
             status = '<td><img data-toggle="tooltip" data-placement="right" title="ERROR: Sensor Invalid" class="bin-alert" width="20px" src = "/static/images/wrench.png"></td';
             percentFilled = 0;
@@ -31,15 +32,18 @@ function processData(){
             barColor = 'bg-warning';
         if(percentFilled > 85){
             barColor = 'bg-danger';
+            icon = 'static/images/trash-red.png'
              status = '<td><img data-toggle="tooltip" data-placement="right" title="Requires Emptying" class="bin-alert" width="20px" src = "/static/images/exclamation-mark.png"></td';
         }
         table.insertAdjacentHTML('beforeend', '<tr><td><b>'+num+'</b></td><td><div class="progress"><div class="progress-bar-striped progress-bar ' + barColor +'" role="progressbar" style="width: '+ percentFilled + '%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div></td><td>'+percentFilled+'%</td>'+status+'</tr>');
         // add bin marker to google map
+
         var marker = new google.maps.Marker({
             position: {lat: bin["lat"],lng: bin["long"]},
             map: map,
             title: bin_ids[i],
             label: {text: num.toString(), color: "white"},
+            icon: icon,
         });
         google.maps.event.addListener(marker, "click", function (event) {
             alert(this.position);
