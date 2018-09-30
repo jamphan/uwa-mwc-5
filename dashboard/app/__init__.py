7c5f6e7e6613966274b9ff0609d0325b260e4a9e
kjph <jamie.kt.phan@gmail.com>
2018-09-30 12:21:15 +0800

    clean dashbord code

--- dashboard/app/__init__.py
+++ dashboard/app/__init__.py
@@ -1,24 +1,57 @@
-from flask import Flask
+import re
+from flask import Flask, render_template
 from flask_mqtt import Mqtt
+
+from app.database.jsonDb import jsonDb
+
+DATABASE_PATH = 'data.json'
+
 flaskApp = Flask(__name__)
-
-from app import routes
-from app import server
-import dash
-
-
 flaskApp.config['MQTT_BROKER_URL'] = 'm2m.eclipse.org'
 
-mqtt = Mqtt(flaskApp)
-mqtt.subscribe('UWA/CITS/#')
+@flaskApp.route('/')
+def index():
 
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    server.addToJSON(data);
+    db = jsonDb(DATABASE_PATH)
+    bin_ids = db.get_all_bins()
+    print(bin_ids)
+    data = db.data
+
+    return render_template('home.html', data=data, bin_ids = bin_ids)
+
+@flaskApp.route('/datalog')
+def datalog():
+    db = jsonDb(DATABASE_PATH)
+
+    bin_ids = db.get_all_bins()
+    sensor_ids = db.get_all_sensors()
+
+    sensorData = db.data["sensors"];
+    binData = db.data["bins"];
+
+    database = db.data["data"]
+
+    return render_template('datalog.html',  bin_data=binData, bin_ids=bin_ids, 
+                                            sensor_ids = sensor_ids, 
+                                            sensor_data = sensorData, data = database)
+@flaskApp.route('/settings')
+def settings():
+    db = jsonDb(DATABASE_PATH)
+    bin_ids = db.get_all_bins()
+    return render_template('settings.html',  bin_ids = bin_ids)
+
+@flaskApp.route('/sensors')
+def sensors():
+    db = jsonDb(DATABASE_PATH)
+    bin_ids = db.get_all_bins()
+
+    # create bin id array
+    options = []
+    options.append({"text":"All Bins", "value": "All bins"})
+    for ids in bin_ids:
+        options.append({"text": "Bin "+ re.search(r'\d+', ids).group(), "value": ids})
+    
+    return render_template('sensors.html', data=db.data, bin_ids = bin_ids, options = options)
+
 if __name__ == '__main__':
-	# app.run_server(debug=True, port=8050, host='127.0.0.1')
 	flaskApp.run_server(debug=True)
--- dashboard/app/server.py
+++ dashboard/app/server.py
@@ -1,15 +1,42 @@
-import json, os, io
+import re
+from flask import Flask, render_template
+from flask_mqtt import Mqtt
 
-def getJSON():
-    with open("app/data.json") as f:
-        data = json.load(f)
-    return(data)  
+from app.database.jsonDb import jsonDb
 
-def addToJSON(data):
-    if os.path.exists("./data.json"):
-        # checks if file exists
-        print ("./data.json File exists and is readable")
-    else:
-        print ("Either file is missing or is not readable, creating file...")
-        with open(os.path.join("./", "data.json"), 'w') as db_file:
-            db_file.write("hi")
+# Constants
+DB_PATH = 'uwa_bins.db'
+MQTT_BROKER_HOST =  'm2m.eclipse.org'
+MQTT_TOPICS = ['UWA/CITS/#']
+
+class BinServer(object):
+    """ Subscribes to the relevant MQTT messages and displays onto Flask App
+    """
+
+    def __init__(self, db_path='uwa_bins.db', db_type='json'):
+
+        self._flask = Flask(__name__)
+        self._flask.config['MQTT_BROKER_URL'] = MQTT_BROKER_HOST
+        self._db_path = db_path
+
+        if db_type == 'json':
+            self._db = jsonDb(path=db_path)
+
+        self._mqtt = Mqtt(self._flask)
+        for topic in MQTT_TOPICS:
+            self._mqtt.subscribe(topic)
+
+    def flask_route(self, func):
+
+        def wrapper_flaskroute():
+        
+                func(self._db)
+        
+        return wrapper_flaskroute
+
+    def flask_run(self, debug=False):
+        self._flask.run_server(debug=debug)
+
+    @property
+    def on_mqtt(self):
+        return self._mqtt.on_message()
