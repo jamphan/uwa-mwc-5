c550cd4fa37bc737552158792b32f3bc8d22683e
kjph <jamie.kt.phan@gmail.com>
2018-10-01 18:47:14 +0800

    Update sql database interface

--- trashyApp/tests/test_sqlDatabase.py
+++ trashyApp/tests/test_sqlDatabase.py
@@ -3,15 +3,85 @@ import os
 
 from trashyApp.database.sqliteDb import SQLite3Db
 
-TEST_PATH = 'test.db'
+@pytest.fixture(scope='module')
+def basedb():
+    """ Create the base database to test from. Ensure that we do not remove
+    _test_data entries. It's oaky to add more data though!
+    """
 
-def test_sqlite3_basic():
+    TEST_PATH = 'basedb_test.db'
     
-    db = SQLite3Db(path=TEST_PATH, build=True)
-    db.add_bin('test_bin_2', position=(10,20))
-    for i in db.get_info_bin('test_bin_2', key='latitude'):
-        print(i)
+    _test_data_bin = { # Bin_Id: [(lat, lon), capacity, fill_threshold] 
+        'perth_bin_1': [(-31.9505, 115.8605), 100, 80],
+        'perth_bin_2': [(-31.9512, 115.8503), 120, 110]
+    }
 
+    # Start fresh
+    if os.path.exists(TEST_PATH):
+        os.remove(TEST_PATH)
+
+    # Create a clean database with fixed test data
+    db = SQLite3Db(path=TEST_PATH, build=True)
+
+    # Add the bin data
+    for bin_id, bin_data in _test_data_bin.items():
+        lat = bin_data[0][0]
+        lon = bin_data[0][1]
+        cap = bin_data[1]
+        thr = bin_data[2]
+        db.add_bin(bin_id, position=(lat,lon), capacity=cap, fill_threshold=thr)
+
+    yield db
+
+    # Clean up
     db.close()
     os.remove(TEST_PATH)
 
+def test_sqlDatabase_noReturn(basedb):
+    """ Check that requesting a bin that does not exist returns None
+    """
+
+    assert basedb.get_info_bin('not_a_bin') is None
+
+def test_sqlDatabase_addElements(basedb):
+    """ Check that the elements (bins and sensors) have been correctly
+    added
+    """
+
+    # Check that a record does exist
+    assert basedb.get_info_bin('perth_bin_1') is not None
+
+    # Check that the format is correct
+    assert basedb.get_info_bin('perth_bin_1') == {'bin_id': 'perth_bin_1',
+                                                  'latitude': -31.9505,
+                                                  'longitude': 115.8605, 
+                                                  'capacity': 100, 
+                                                  'fill_threshold': 80}
+
+def test_sqlDatabase_isBinMethod(basedb):
+    """ Check the method is_bin() works
+    """
+
+    # Check if can pick an actual bin
+    assert basedb.is_bin('perth_bin_1')
+
+    # Check it detects non-bins
+    assert not(basedb.is_bin('not_a_bin'))
+
+def test_sqlDatabase_repeatedAddFails(basedb):
+    """ Check that the database does not permit adding non-unique bin labels
+    """
+
+    assert basedb.add_bin('perth_bin_1') == False
+
+def test_sqlDatabase_getAllBinsMethod(basedb):
+
+    expected = ['perth_bin_1', 'perth_bin_2']
+    actual = basedb.get_all_bins()
+    assert len(expected) == len(actual)
+    
+    for bin_id in actual:
+        assert bin_id in expected
+
+    for bin_id in expected:
+        assert bin_id in actual
--- trashyApp/trashyApp/database/sqliteDb.py
+++ trashyApp/trashyApp/database/sqliteDb.py
@@ -1,14 +1,30 @@
+from __future__ import with_statement
+
 import sqlite3
 import os
+from contextlib import closing
 
 from . import BaseBinDb
 
 class SQLite3Db(BaseBinDb):
+    """ A sql implementation of the base bin database ABC
 
-    def __init__(self, path='database.db', build=False):
+    Args:
+        path (str, optional): The path that the database should be looaded from
+            defaults to database.db relative to the instance
+        build (bool, optional): Specify True to overwrite any existing data and
+            rebuild the entire schema
+
+    Attributes:
+        conn (sqlite3.connection): The open sqlite connection
+    """
+
+    def __init__(self, path='./database.db', build=False):
 
         self._path = path
 
+        # If the user requests to build, we will clear any existing
+        # db files and make a new file
         if build:
             if os.path.exists(self._path):
                 os.remove(self._path)
@@ -21,21 +37,35 @@ class SQLite3Db(BaseBinDb):
         conn = sqlite3.connect(self._path)
         self._conn = conn
 
+        # TODO: Check if schema is valid
+
     @property
     def conn(self):
         return self._conn
 
-    @property
-    def cursor(self):
-        return self._conn.cursor()
-
     def close(self):
+        """ Cleanly close the database
+        """
+
         self._conn.close()
 
     def committed(func):
+        """ All queries in this class should use this decorator, as it will
+        correctly wrap the cursor and commit.
+
+        Once decorated, the self.cursor property will be available for use.
+
+        Note because this is set to use the context with an attribute,
+        we cannot nest committed functions (the nested function will
+        close self.cursor, and the parent function will fail)
+
+        Todo:
+            investigate nested funcitonality
+        """
 
         def wrapper_committed(self, *args, **kwargs):
-            func(self, *args, **kwargs)
+            with closing(self._conn.cursor()) as self.cursor:
+                return func(self, *args, **kwargs)
             self.conn.commit()
 
         return wrapper_committed
@@ -43,6 +73,7 @@ class SQLite3Db(BaseBinDb):
     @committed
     def _schema_build(self):
 
+        # TODO: Add unique for label
         self.cursor.execute("""CREATE TABLE Item
         (
             id INTEGER primary key autoincrement
@@ -50,50 +81,117 @@ class SQLite3Db(BaseBinDb):
             ,type TEXT
             ,latitude REAL
             ,longitude REAL
+            ,capacity REAL
+            ,fill_threshold REAL
         )
         """)
 
-        # self.cursor.execute("""CREATE TABLE Sensor
-        # (
-        #     id INTEGER primary key autoincrement
-        #     ,label TEXT
-        #     ,type TEXT
-        #     ,ItemId INTEGER
-        #     ,FOREIGN KEY(ItemId) REFERENCES 
-        # )
-        # """)
+        self.cursor.execute("""CREATE TABLE Sensor
+        (
+            id INTEGER primary key autoincrement
+            ,label TEXT
+            ,type TEXT
+            ,ItemId INTEGER
+            ,FOREIGN KEY(ItemId) REFERENCES Item(id)
+        )
+        """)
 
     @committed
     def add_bin(self, bin_id, position=None, capacity=None, fill_threshold=None):
-        
+        """ Adds a bin to the database
+
+        Args:
+            bin_id (str): the bin identifier (label) to add to the database
+            position (tuple, default=None): 
+
+        Returns:
+            bool: True on success, False otherwise
+        """
+
+        # TODO: Permit a bulk insert using .executemany()
+        #       - Check that len of all parameters are equal
+
+        # Check if the label already exists
+        # TODO: Check if _schema_build() has unique constraint
+        self.cursor.execute("""SELECT id FROM Item WHERE label = ?""", (bin_id,))
+        if self.cursor.fetchone() is not None:
+            return False
+
+        # The lat, long need to default to two serparate params
         if isinstance(position, tuple) and len(position) == 2:
             lat, long = position
         else:
             lat = None
             long = None
-        vals = (bin_id, 'bin', lat, long, )
 
-        self.cursor.execute("""INSERT INTO Item
-        (
-            label
-            ,type
-            ,latitude
-            ,longitude
-        )
-        VALUES
-        (
-            ?
-            ,?
-            ,?
-            ,?
-        )
-        """, vals)
+        # Prepare the fill
+        vals = (bin_id, 'bin', lat, long, capacity, fill_threshold)
 
-    def get_info_bin(self, bin_id, key=None):
-        
-        if key is not None:
-            vals = (key, bin_id, )
-            return self.cursor.execute("""SELECT ? FROM Item WHERE label = ?""", vals)
+        self.cursor.execute("""INSERT INTO Item (label, type, latitude, longitude, capacity, fill_threshold)
+        VALUES(?,?,?,?,?,?)""", vals)
+
+        return True
+
+    @committed
+    def get_info_bin(self, bin_id):
+        """ Returns the information for a single bin_id
+
+        Note:
+            We do not need to check for multiple records as there is a unique
+            constraint on the label. We only need to check for no results
+
+        Args:
+            bin_id (str): The bin to request information for
+
+        Returns:
+            dict: With keys ['bin_id', 'latitude', 'longitude', 'capacity', 'fill_threshold']
+            None: if any errors
+        """
+
+        vals = (bin_id, )
+        results = self.cursor.execute("""SELECT * FROM Item WHERE label = ?""", vals)
+
+        # Do a quick check that we don't have an empty result set
+        all_results = [x for x in results]
+        if len(all_results) == 0:
+            return None
         else:
-            vals = (bin_id, )
-            return self.cursor.execute("""SELECT * FROM Item WHERE label = ?""", vals)
+            ret = dict(
+                bin_id=all_results[0][1],
+                latitude=all_results[0][3],
+                longitude=all_results[0][4],
+                capacity=all_results[0][5],
+                fill_threshold=all_results[0][6]
+            )
+
+        return ret
+
+    @committed
+    def is_bin(self, test_id):
+        """ Test whether the request id is a bin or not
+
+        Args:
+            test_id (str): The item to test
+
+        Returns:
+            bool: True if test_id is a bin in the database, False otherwise
+        """
+
+        vals = (test_id, )
+        self.cursor.execute("""SELECT id FROM Item WHERE label = ?""", vals)
+        if self.cursor.fetchone() is not None:
+            return True
+        else:
+            return False
+
+    @committed
+    def get_all_bins(self):
+        """ Get all the ids of the bins registerd in database
+
+        Returns:
+            list: A list of all bin ids
+        """
+
+        results = self.cursor.execute("""SELECT label FROM Item""")
+        all_results = [x[0] for x in results]
+        return all_results
